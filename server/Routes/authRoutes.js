c77875978286a10dd561986eb91d967123f4f90e
rajeshkanna <rajeshkannakbd@gmail.com>
2025-07-16 11:08:35 +0530

    blog rotes and loginroutes

---
+++ server/Routes/authRoutes.js
@@ -0,0 +1,42 @@
+const express =require('express');
+const UserModel = require('../Models/userModel');
+const router = express.Router();
+const bycrpt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const jwt_secret_key = process.env.jwt_secret_key
+
+
+router.post("/register",async(req,res)=>{
+  try{
+ const {name,email,password} = req.body;
+ const hashedpass = bycrpt.hashSync(password,10)
+  const user = await UserModel.create({name,email,password:hashedpass})
+  const token = jwt.sign({id:user._id,email:user.email},jwt_secret_key,{expiresIn:'3h'})
+  res.status(201).json({user:user,token:token})
+  }
+  catch(err){
+    res.status(500).json("error on registering")
+  }
+})
+
+router.post("/login",async(req,res)=>{
+  try{
+  const {email , password} =req.body
+  const user = await UserModel.findOne({email})
+  if(!user){
+   return res.status(404).json({message:"no user exists"})
+  }
+  const ismatch = await bycrpt.compare(password,user.password)
+  if(!ismatch){
+   return res.status(401).json({message:"incoorect password"})
+  }
+  const token = jwt.sign({id:user._id,email:user.email},jwt_secret_key,{expiresIn:'3h'})
+  res.status(200).json({user:user,token:token})
+  }catch(err){
+    res.status(500).json({message:"login failed"})
+  }
+  
+  
+})
+
+module.exports = router;
--- server/Routes/postRoutes.js
+++ server/Routes/postRoutes.js
@@ -1,8 +1,61 @@
-const express = require('express')
+const express = require('express');
+const postModel = require('../Models/postModel');
 const router = express.Router();
 
-router.get("/posts", (req, res) => {
-  res.send("Hello from posts route");
+router.post("/newblog",async(req,res)=>{
+   try {
+    const newpost = new postModel( req.body)
+    const savedpost = await newpost.save()
+    res.json(savedpost).status(200)
+    
+   } catch (error) {
+    console.log(error);
+    res.json(err).status(500)
+   }
+})
+
+router.get("/", async (req, res) => {
+  const post = await postModel.find({})
+  res.status(200).json(post)
+})
+
+router.get("/:id",async(req,res)=>{
+  const id = req.params.id
+  try {
+    const post = await postModel.findById(id)
+    if(!post){
+      res.status(404).json("post not found")
+    }
+    res.status(200).json(post)
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+})
+
+router.delete("/:id",async(req,res)=>{
+  const id = req.params.id
+  try {
+    const post = await postModel.findByIdAndDelete(id)
+    res.status(200).json({message:"post get deleted"})
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+})
+
+router.put("/:id",async(req,res)=>{
+try{
+  const id = req.params.id
+   const newpost = req.body
+   const updatedpost = await postModel.findByIdAndUpdate(id,newpost)
+  res.status(200).json(updatedpost)
+}catch(err){
+  console.log(err);
+  res.status(500).json({message:"error on updating the blog"})
+}  
 })
 
 module.exports = router;
